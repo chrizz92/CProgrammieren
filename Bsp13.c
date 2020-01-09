0e48b0e7748bae533f5d63ee3698f62081065d96
chrizz92 <christianschadler@gmx.at>
2020-01-09 19:52:48 +0100

    Taschenrechner

---
+++ Bsp13.c
@@ -0,0 +1,39 @@
+float numberOne, numberTwo, result;
+
+int readNumbers(float nr1, float nr2) {
+	printf("Bitte 1. Zahl eingeben: ");
+	scanf("%f", &nr1);
+	printf("Bitte 2. Zahl eingeben: ");
+	scanf("%f", &nr2);
+	return 0;
+}
+
+int add(float nr1, float nr2, float *res) {
+	return 0;
+}
+
+int sub(float nr1, float nr2, float *res) {
+	return 0;
+}
+
+int mul(float nr1, float nr2, float *res) {
+	return 0;
+}
+
+int div(float nr1, float nr2, float *res) {
+	return 0;
+}
+
+
+
+//MAIN PROGRAM
+
+int main(void) {
+
+	printf("Taschenrechner\n");
+	printf("==============\n");
+	readNumbers(numberOne, numberTwo);
+
+
+	return 0;
+}
